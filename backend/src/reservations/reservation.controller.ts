2dfe352850caec81d048961f3fddfcd364b73fb2
GregMagnat <magnat.gregory@gmail.com>
2024-05-02 13:28:57 +0200

    controller bdd resa

---
+++ backend/src/reservations/reservation.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { ReservationService } from './reservation.service';
+
+@Controller('reservations')
+export class ReservationController {
+  constructor(private readonly reservationService: ReservationService) {}
+
+  @Get()
+  getAllReservations() {
+    return this.reservationService.getAllReservations();
+  }
+
+  @Get(':id')
+  getReservationById(@Param('id') id: number) {
+    return this.reservationService.getReservationById(id);
+  }
+
+  @Post()
+  createReservation(@Body() createReservationDto: CreateReservationDto) {
+    return this.reservationService.createReservation(createReservationDto);
+  }
+
+  @Put(':id')
+  updateReservation(@Param('id') id: number, @Body() updateReservationDto: UpdateReservationDto) {
+    return this.reservationService.updateReservation(id, updateReservationDto);
+  }
+
+  @Delete(':id')
+  deleteReservation(@Param('id') id: number) {
+    return this.reservationService.deleteReservation(id);
+  }
+}
