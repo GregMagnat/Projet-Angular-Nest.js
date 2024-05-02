e2c3d6bc69c90784effb9f6ea455d2c608f30721
GregMagnat <magnat.gregory@gmail.com>
2024-05-02 11:37:00 +0200

    table category and reserevation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reservation } from './reservations/reservation.entity';
+import { Category } from './categorys/category.entity'; 
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [Reservation, Category], 
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
