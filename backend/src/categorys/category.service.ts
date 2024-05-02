a6444f0d4c8fb27ede8db17195e65b0928169de4
GregMagnat <magnat.gregory@gmail.com>
2024-05-02 15:43:24 +0200

    bdd final

---
+++ backend/src/categorys/category.service.ts
@@ -0,0 +1,12 @@
+// category.service.ts
+
+import { Injectable } from '@nestjs/common';
+import { Category } from './category.entity';
+import { CATEGORIES } from './category.entity'; 
+
+@Injectable()
+export class CategoryService {
+  getAllCategories(): Category[] {
+    return CATEGORIES;
+  }
+}
--- backend/src/tables/table.entity.ts
+++ backend/src/tables/table.entity.ts
@@ -12,3 +12,14 @@ export class Table {
     @OneToMany(() => Reservation, reservation => reservation.table)
     reservations: Reservation[];
 }
+
+export const TABLES: Partial<Table>[] = [
+    { id: 1, number: 1 },
+    { id: 2, number: 2 },
+    { id: 3, number: 3 },
+    { id: 4, number: 4 },
+    { id: 5, number: 5 },
+    { id: 6, number: 6 },
+    { id: 7, number: 7 },
+    { id: 8, number: 8 }
+];
