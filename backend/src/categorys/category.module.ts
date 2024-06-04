e94861beb2ae868e404dcbfa621f1eac3d99c531
GregMagnat <magnat.gregory@gmail.com>
2024-06-04 15:36:59 +0200

    Validation de la bdd

--- backend/src/categorys/category.module.ts
+++ backend/src/categorys/category.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CategoryController } from './category.controller';
+import { CategoryService } from './category.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './category.entity';
-import { CategoryService } from './category.service';
-import { CategoryInitializerService } from './category-initializer.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
-  providers: [CategoryService, CategoryInitializerService],
+  controllers: [CategoryController], 
+  providers: [CategoryService],
   exports: [CategoryService],
 })
 export class CategoryModule {}
