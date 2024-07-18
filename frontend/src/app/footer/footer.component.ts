e50612dde8f923a73a092486fe0f4890ecec8884
GregMagnat <magnat.gregory@gmail.com>
2024-07-18 11:52:26 +0200

    footer component

---
+++ frontend/src/app/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      footer works!
+    </p>
+  `,
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
