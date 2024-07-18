d1ff68424f0d0a7f4660c859d105b16fafae2c2c
LouisKzy <louiskrejezy@gmail.com>
2024-07-18 11:49:58 +0200

    Nav bar component + App component with footer & navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterModule],
-  template: ` <router-outlet></router-outlet> `,
+  imports: [HomeComponent, RouterModule, NavbarComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
+  `,
   styleUrl: './app.component.css',
 })
 export class AppComponent {
---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      navbar works!
+    </p>
+  `,
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
