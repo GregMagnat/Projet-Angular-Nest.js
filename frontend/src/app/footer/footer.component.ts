77e0ab3f9b67a6b14cb04a7daf9d11a8272f3920
LouisKzy <louiskrejezy@gmail.com>
2024-07-19 11:21:08 +0200

    backup

---
+++ frontend/src/app/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  template: ` <p>Salut Kamire !</p> `,
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
