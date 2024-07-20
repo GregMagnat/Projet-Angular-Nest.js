a0953796a67438a19c61dad2cd6bf589ebfac552
GregMagnat <magnat.gregory@gmail.com>
2024-07-20 11:47:05 +0200

    Router

---
+++ frontend/src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      about works!
+    </p>
+  `,
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home Page' },
+  { path: 'reservation', component: ReservationComponent, title: 'Reservation' },
+  { path: 'gallery', component: GalleryComponent, title: 'Gallery' },
+  { path: 'about', component: AboutComponent, title: 'About' },
 ];
 
 export { routes };
