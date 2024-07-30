be4b92f6aaaa5b6ec18eaf5e3eb3c9af2f02d51c
GregMagnat <magnat.gregory@gmail.com>
2024-07-30 13:05:06 +0200

    begin calendar

---
+++ frontend/src/app/calendar/calendar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      calendar works!
+    </p>
+  `,
+  styleUrl: './calendar.component.css'
+})
+export class CalendarComponent {
+
+}
