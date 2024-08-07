17b00840b57748ab954b0823c159a9df5ec229f2
LouisKzy <louiskrejezy@gmail.com>
2024-08-07 15:41:35 +0200

    ✨ admin scheduler

--- frontend/src/app/admin-calendar/admin-calendar.component.ts
+++ frontend/src/app/admin-calendar/admin-calendar.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Inject,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import {
   CalendarEvent,
   CalendarMonthViewDay,
@@ -6,6 +12,15 @@ import {
 } from 'angular-calendar';
 import { CommonModule } from '@angular/common';
 import { AdminCalendarModule } from './admin-calendar.module';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { LOCALE_ID } from '@angular/core';
+import { ReservationService } from '../reservation.service';
+import { Reservation } from '../reservation.model';
+import { format } from 'date-fns';
+import { fr } from 'date-fns/locale';
+
+registerLocaleData(localeFr);
 
 @Component({
   selector: 'app-admin-calendar',
@@ -19,6 +34,9 @@ import { AdminCalendarModule } from './admin-calendar.module';
           <button (click)="onToday()">Aujourd'hui</button>
           <button (click)="increment()">Prochain</button>
         </div>
+        <div class="current-date">
+          <span>{{ viewDate | date : 'fullDate' }}</span>
+        </div>
         <div class="button-group">
           <button (click)="setView(CalendarViews.Month)">Mois</button>
           <button (click)="setView(CalendarViews.Week)">Semaine</button>
@@ -26,61 +44,114 @@ import { AdminCalendarModule } from './admin-calendar.module';
         </div>
       </div>
 
-      <ng-template #customHeaderTemplate let-days="days">
-        <span>{{ viewDate | date : 'MMMM y' }}</span>
-      </ng-template>
-
       <ng-container [ngSwitch]="view">
         <mwl-calendar-month-view
           *ngSwitchCase="CalendarViews.Month"
           [viewDate]="viewDate"
           [events]="events"
-          [headerTemplate]="customHeaderTemplate"
           (dayClicked)="onDayClicked($event)"
+          [locale]="'fr'"
+          [activeDayIsOpen]="true"
         ></mwl-calendar-month-view>
 
         <mwl-calendar-week-view
           *ngSwitchCase="CalendarViews.Week"
           [viewDate]="viewDate"
           [events]="events"
-        ></mwl-calendar-week-view>
+          [hourSegments]="1"
+          [hourSegmentHeight]="50"
+          [dayStartHour]="10"
+          [dayEndHour]="20"
+          [locale]="'fr'"
+        >
+          <!-- Custom hour segment formatting -->
+          <ng-template #hourTemplate let-hour="hour">
+            <div class="custom-hour-segment">
+              {{ hour | date : 'HH:mm' }}
+            </div>
+          </ng-template>
+        </mwl-calendar-week-view>
 
         <mwl-calendar-day-view
           *ngSwitchCase="CalendarViews.Day"
           [viewDate]="viewDate"
           [events]="events"
-        ></mwl-calendar-day-view>
+          [hourSegments]="1"
+          [hourSegmentHeight]="60"
+          [dayStartHour]="10"
+          [dayEndHour]="20"
+          [locale]="'fr'"
+        >
+          <!-- Custom hour segment formatting -->
+          <ng-template #hourTemplate let-hour="hour">
+            <div class="custom-hour-segment">
+              {{ hour | date : 'HH:mm' }}
+            </div>
+          </ng-template>
+        </mwl-calendar-day-view>
       </ng-container>
     </div>
   `,
   styleUrls: ['./admin-calendar.component.css'],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr' }],
 })
 export class AdminCalendarComponent implements OnInit {
   CalendarViews = CalendarView;
   view: CalendarView = CalendarView.Month;
   viewDate: Date = new Date();
   selectedDate: Date | null = null;
+  events: CalendarEvent[] = [];
 
-  events: CalendarEvent[] = [
-    {
-      start: new Date(),
-      title: 'Événement 1',
-    },
-    {
-      start: new Date(),
-      title: 'Événement 2',
-    },
-    {
-      start: new Date(new Date().setDate(new Date().getDate() + 1)),
-      title: 'Événement 3',
-    },
-  ];
+  @ViewChild('hourTemplate') hourTemplate!: TemplateRef<any>;
+
+  constructor(
+    @Inject(LOCALE_ID) private locale: string,
+    private reservationService: ReservationService
+  ) {}
 
   ngOnInit(): void {
     this.selectedDate = new Date();
+    this.setView(this.view);
+    this.reservationService.getReservations().subscribe({
+      next: (reservations: Reservation[]) => {
+        this.events = this.mapReservationsToEvents(reservations);
+        console.log('Events:', this.events);
+      },
+      error: (err) => {
+        console.error('Erreur lors de la récupération des réservations:', err);
+      },
+    });
   }
 
-  get filteredEvents(): CalendarEvent[] {
+  mapReservationsToEvents(reservations: Reservation[]): CalendarEvent[] {
+    return reservations.map((reservation) => {
+      const startDateTime = `${reservation.date.split('T')[0]}T${
+        reservation.hour_start
+      }`;
+      const endDateTime = `${reservation.date.split('T')[0]}T${
+        reservation.hour_end
+      }`;
+      const start = new Date(startDateTime);
+      const end = new Date(endDateTime);
+
+      if (isNaN(start.getTime()) || isNaN(end.getTime())) {
+        console.error('Invalid Date:', startDateTime, endDateTime);
+      }
+
+      return {
+        start,
+        end,
+        title: `${
+          reservation.lastName.charAt(0).toUpperCase() +
+          reservation.lastName.slice(1)
+        } ${
+          reservation.name.charAt(0).toUpperCase() + reservation.name.slice(1)
+        } Téléphone: ${reservation.phone} ${reservation.category_name}`,
+      };
+    });
+  }
+
+  getfilteredEvents(): CalendarEvent[] {
     return this.selectedDate
       ? this.events.filter(
           (event) =>
@@ -91,11 +162,13 @@ export class AdminCalendarComponent implements OnInit {
 
   onDayClicked({ day }: { day: CalendarMonthViewDay }): void {
     this.selectedDate = day.date;
+    this.viewDate = this.selectedDate;
   }
 
   onToday(): void {
     this.viewDate = new Date();
     this.selectedDate = new Date();
+    this.setView(this.view);
   }
 
   increment(): void {
@@ -132,5 +205,10 @@ export class AdminCalendarComponent implements OnInit {
 
   setView(view: CalendarView): void {
     this.view = view;
+    this.viewDate = this.selectedDate || new Date();
+  }
+
+  hourFormatter(date: Date): string {
+    return format(date, 'HH:mm', { locale: fr });
   }
 }
--- frontend/src/app/admin-calendar/admin-calendar.module.ts
+++ frontend/src/app/admin-calendar/admin-calendar.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { LOCALE_ID } from '@angular/core';
+
+// Enregistrer les locales françaises
+registerLocaleData(localeFr);
 
 @NgModule({
   imports: [
@@ -12,5 +18,6 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
     }),
   ],
   exports: [CalendarModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr' }],
 })
 export class AdminCalendarModule {}
