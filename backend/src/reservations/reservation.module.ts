4a2c4d26bbc52443cf415479769a17479bd35e6b
GregMagnat <magnat.gregory@gmail.com>
2024-08-08 15:22:07 +0200

    LimitResa

--- backend/src/reservations/reservation.module.ts
+++ backend/src/reservations/reservation.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reservation } from './reservation.entity';
-import { ReservationController } from './reservation.controller';
 import { ReservationService } from './reservation.service';
+import { ReservationController } from './reservation.controller';
 import { TableModule } from '../tables/table.module';
+import { CategoryModule } from '../categorys/category.module';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Reservation]), TableModule],
-  controllers: [ReservationController],
+  imports: [
+    TypeOrmModule.forFeature([Reservation]),
+    TableModule,
+    CategoryModule,
+  ],
   providers: [ReservationService],
+  controllers: [ReservationController],
 })
 export class ReservationModule {}
--- backend/src/tables/table.entity.ts
+++ backend/src/tables/table.entity.ts
@@ -3,23 +3,24 @@ import { Reservation } from '../reservations/reservation.entity';
 
 @Entity()
 export class Table {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    number: number; 
+  @Column()
+  number: number;
 
-    @OneToMany(() => Reservation, reservation => reservation.table)
-    reservations: Reservation[];
+  @OneToMany(() => Reservation, (reservation) => reservation.table)
+  reservations: Reservation[];
 }
 
 export const TABLES: Partial<Table>[] = [
-    { id: 1, number: 1 },
-    { id: 2, number: 2 },
-    { id: 3, number: 3 },
-    { id: 4, number: 4 },
-    { id: 5, number: 5 },
-    { id: 6, number: 6 },
-    { id: 7, number: 7 },
-    { id: 8, number: 8 }
+  { id: 1, number: 1 },
+  { id: 2, number: 2 },
+  { id: 3, number: 3 },
+  { id: 4, number: 4 },
+  { id: 5, number: 5 },
+  { id: 6, number: 6 },
+  { id: 7, number: 7 },
+  { id: 8, number: 8 },
+  { id: 9, number: 9 },
 ];
