6c7403504cd23a5443db1be4080b378784773aa6
100PBaguuette <magnat.gregory@gmail.com>
2024-09-06 21:57:51 +0200

    PushPutty

---
+++ frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    authUsername: 'Bast26',
+    authPassword: 'Bastien.Touja.26@',
+  };
---
+++ frontend/src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    authUsername: 'Bast26',
+    authPassword: 'Bastien.Touja.26@',
+  };
